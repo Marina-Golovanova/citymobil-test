a02e9a6f5da48a2399b0db1c2c87d1892e1307f4
Marina <gma18@list.ru>
2021-07-22 14:22:04 +0300

    feat: add button for scroll to top

--- src/components/main-content/MainContent.tsx
+++ src/components/main-content/MainContent.tsx
@@ -2,10 +2,19 @@ import React from "react";
 import { InputSearch } from "../input-search/InputSearch";
 import { Button } from "../button/Button";
 import { Table } from "../table/Table";
+import { ScrollToTopButton } from "../scroll-to-top-button/ScrollToTopButton";
 
 import "./main-content.scss";
 
 export const MainContent: React.FC = () => {
+  const [showButtonForScroll, setShowButtonToScroll] = React.useState(false);
+
+  window.onscroll = () => {
+    window.scrollY > 300
+      ? setShowButtonToScroll(true)
+      : setShowButtonToScroll(false);
+  };
+
   return (
     <div className="main-content">
       <div className="main-content__search-layout">
@@ -15,6 +24,7 @@ export const MainContent: React.FC = () => {
         </div>
       </div>
       <Table />
+      {showButtonForScroll && <ScrollToTopButton />}
     </div>
   );
 };
